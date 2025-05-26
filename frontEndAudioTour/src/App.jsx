1e1f383bb796e594ffecf173153b41ccdb787441
olevanderheiden <olevanderheiden@hotmail.com>
2025-05-26 16:40:23 +0200

    V0.5
    Added the DT Logo And changed the header to be as much in line with the one used by DT as I was capable of doing in my limited styling ability!

--- frontEndAudioTour/src/App.jsx
+++ frontEndAudioTour/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import AudioTourList from "./assets/AudioTourList";
+import logo from "../public/DT-LogoSide-Primary-Dark.svg";
 
 function App() {
   return (
@@ -8,15 +9,43 @@ function App() {
       {/* Navigation/Header */}
       <header className="header">
         <div className="header-content">
-          <div className="logo">
-            {/* Replace with your logo or site name */}
-            <span>Afstudeerportfolio</span>
+          <div className="logo-block">
+            <img
+              src={logo}
+              alt="Digitaal Toegankelijk logo"
+              className="header-logo"
+            />
+            <span className="header-title">
+              Digitaal
+              <br />
+              Toegankelijk
+            </span>
           </div>
           <nav className="nav-links">
-            {/* Example navigation links */}
-            <a href="#audio-tour">Audio Tour</a>
-            <a href="#over">Over</a>
-            <a href="#contact">Contact</a>
+            <a href="#trainingen">Trainingen</a>
+            <a href="https://digitaaltoegankelijk.nl/diensten/toegankelijkheidsonderzoeken-audits/">
+              Audits
+            </a>
+            <a href="https://digitaaltoegankelijk.nl/diensten/advies/">
+              Advies
+            </a>
+            <a href="https://digitaaltoegankelijk.nl/nieuws/">Nieuws</a>
+            <a href="#" className="active-link">
+              Over ons
+            </a>
+            <a href="https://digitaaltoegankelijk.nl/contact/">Contact</a>
+            <a
+              href="https://digitaaltoegankelijk.nl/whitepapers/"
+              className="cta-btn"
+            >
+              Gratis Whitepapers
+            </a>
+            <a
+              href="https://digitaaltoegankelijk.nl/zoeken/"
+              className="search-link"
+            >
+              Zoeken
+            </a>
           </nav>
         </div>
       </header>
