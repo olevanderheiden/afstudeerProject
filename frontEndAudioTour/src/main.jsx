d998f0f8b65b95d478487d30e262b09a3cf826f6
olevanderheiden <olevanderheiden@hotmail.com>
2025-06-02 16:13:19 +0200

    V0.16
    Adding suport for serviceWorker in production builds reducing load times (hopefully). This still needs to be tested due to not working as intended in dev builds.

--- frontEndAudioTour/src/main.jsx
+++ frontEndAudioTour/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
