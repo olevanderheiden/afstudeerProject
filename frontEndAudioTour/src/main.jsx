36d8636d94e9c028b5d595ba0abd7aad450872f4
olevanderheiden <olevanderheiden@hotmail.com>
2025-06-03 16:25:14 +0200

    V0.17.1
    Small patch to improve load process from cach

--- frontEndAudioTour/src/main.jsx
+++ frontEndAudioTour/src/main.jsx
@@ -5,9 +5,23 @@ import App from "./App.jsx";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register(
-      `${import.meta.env.BASE_URL}service-worker.js`
-    );
+    navigator.serviceWorker
+      .register(`${import.meta.env.BASE_URL}service-worker.js`)
+      .then((reg) => {
+        reg.addEventListener("updatefound", () => {
+          const newWorker = reg.installing;
+          if (newWorker) {
+            newWorker.addEventListener("statechange", () => {
+              if (
+                newWorker.state === "installed" &&
+                navigator.serviceWorker.controller
+              ) {
+                window.location.reload();
+              }
+            });
+          }
+        });
+      });
   });
 }
 
